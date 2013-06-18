fe422c814ce1521b49f7d036d688dc9e66f5e08c
noxiouz <noxiouz@yandex.ru>
2013-06-18 19:31:23 +0400

    debianization

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name="cocaine_tornado_proxy",
+    version="0.10.5",
+    url="https://github.com/cocaine/cocaine-Tornado-proxy",
+    author="Anton Tyurin",
+    author_email="noxiouz@yandex.ru",
+    license="LGPLv3+",
+    scripts=['cocaine-tornado-proxy']
+)
